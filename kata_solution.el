f493384e9d1ba6d22e21ca229d3c5e3b35c7aa4f
Caz <caz@rover.com>
2018-05-24 23:55:38 -0700

    update kata-el

--- kata_solution.el
+++ kata_solution.el
@@ -1,15 +1,23 @@
 -module(kata_solution).
 -export([add/1]).
+-import(string, [replace/4, split/3]).
 -include_lib("eunit/include/eunit.hrl").
 
 add("") -> 0;
-add("//" + Delim + "\n" + Nums) -> add(Nums, Delim);
-add(Nums) -> add(Nums, "");
-add(Nums, CustomDelim) ->
-    NumList = string:lexemes(Nums, CustomDelim ++ "," ++ [$\n]),
-    lists:foldl(fun(X, Sum) -> 
-        Sum + string:to_integer(X) 
-    end, 0, NumList).
+add("//" + CustomDelim + "\n" + Input) ->
+    CustomDelimsReplaced = replace(Input, CustomDelim, ",", all),
+    add(CustomDelimReplaced);
+add (Input) ->
+    NewlinesReplaced = replace(Input, "\n", ",", all),
+    addNums(NewlinesReplaced).
+
+addNums(Nums) ->
+    NumList = split(NewlinesReplaced, ",", all),
+    addNums(0, NumList);
+addNums(Total, []) ->
+    Total;
+addNums(Total, [Num | Nums]) ->
+    add(Total + Num, Nums).
 
 add_test_() ->
     [?assert(add("") =:= 0),
@@ -19,3 +27,22 @@ add_test_() ->
      ?assert(add("5,6\n7,8\n9") =:= 35),
      ?assert(add("//;\n1;2;3") =:= 6)
     ].
+
+add_1("") -> 0;
+add_1("//" + CustomDelim + "\n" + Input) ->
+    add_1(Input, [",", [\n], CustomDelim]);
+add_1(Input) ->
+    add_1(Input, [",", [\n]]);
+add_1(Input, Delims) ->
+    NumList = string:lexemes(Input, Delims),
+    addNums(NumList).
+
+add_2("") -> 0;
+add_2("//" + Delim + "\n" + Nums) -> add_2(Nums, Delim);
+add_2(Nums) -> add_2(Nums, "");
+add_2(Nums, CustomDelim) ->
+    NumList = string:lexemes(Nums, CustomDelim ++ "," ++ [$\n]),
+    lists:foldl(fun(X, Sum) -> 
+        Sum + string:to_integer(X) 
+    end, 0, NumList).
+
